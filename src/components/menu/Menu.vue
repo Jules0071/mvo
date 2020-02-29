ed42dc8ad6d24c15b08a8e7b958a3182b70ae83b
julianmungenast <julian.mungenast@intellishop.ag>
2020-02-29 21:30:09 +0100

    menu css

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,10 +1,17 @@
 <template>
     <nav class="mv-menu">
-        <ul>
+        <ul class="mv-menu--list">
             <li v-for="item of items" :key="item.name">
                 <a :href="item.id">
                     <span>{{ item.name }}</span>
                 </a>
+                <ul class="mv-menu--dropdown" v-if="item.children">
+                    <li v-for="i of item.children" :key="i.name">
+                        <a :href="i.id">
+                            <span>{{ i.name }}</span>
+                        </a>
+                    </li>
+                </ul>
             </li>
 
         </ul>
